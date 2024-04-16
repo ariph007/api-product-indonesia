caa792753f1a3a427b0ed274bb6944cea796d03f
Ari Prayoga Hutabarat <ariprayoga007@gmail.com>
2024-04-16 21:25:13 +0700

    add properties

--- src/main/java/com/alpha/products/AlphaProducts/AlphaProductsApplication.java
+++ src/main/java/com/alpha/products/AlphaProducts/AlphaProductsApplication.java
@@ -2,9 +2,10 @@ package com.alpha.products.AlphaProducts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AlphaProductsApplication {
+public class AlphaProductsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlphaProductsApplication.class, args);
